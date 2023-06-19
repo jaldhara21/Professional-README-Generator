0700a74c1a2adc51ae743aeacd607b8b2adf9ff0
jaldhara.patel <jaldharapatel91@gmail.com>
2023-06-18 21:49:45 -0400

    added code to generatemarkdown.js file and create a new readme.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Professional-README-Generator
+# Professional README Generator 
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+
+  A command-line application that dynamically generates a professional README.md from a user's input using the Inquirer package.
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  Download/ clone the Github repo and install dependancies using ```npm install```
+  
+  ## Usage
+  
+  Using the terminal type node command to prompt the program and answer a series of questions.
+  
+  ## License
+
+This application is covered under the [MIT](https://opensource.org/licenses/MIT) license.
+  
+  ## Contributing
+  
+  Please submit contributions to the email below
+  
+  ## Tests
+  
+  N/A
+  
+  ## Questions
+  
+  For additional questions, you can reach me through:
+  
+  - GitHub: [jaldhara21](https://github.com/jaldhara21)
+  - Email: jaldharapatel91@gmail.com
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,93 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPL') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'ISC') {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache') {
+    return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPL') {
+    return '[GPL v3](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'ISC') {
+    return '[ISC](https://opensource.org/licenses/ISC)';
+  } else {
+    return '';
+  }
+}
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license && license !== 'None') {
+    return `## License
+
+This application is covered under the ${renderLicenseLink(license)} license.`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
 
-`;
-}
+  ## Description
+
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  For additional questions, you can reach me through:
+  
+  - GitHub: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email}
+  `;
+  }
 
 module.exports = generateMarkdown;
